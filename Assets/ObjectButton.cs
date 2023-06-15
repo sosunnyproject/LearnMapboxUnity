5fcbf68995955c732dd726c84f41228723a2e527
sosunnyproject <parksosun1103@gmail.com>
2023-06-15 15:52:04 +0900

    add animal pins

---
+++ Assets/ObjectButton.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class ObjectButton : MonoBehaviour
+{
+
+    public GameObject definedButton;
+    public UnityEvent OnClick = new UnityEvent();
+
+    // Use this for initialization
+    void Start()
+    {
+        definedButton = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit Hit;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(ray, out Hit) && Hit.collider.gameObject == gameObject)
+            {
+                Debug.Log("Button Clicked");
+                OnClick.Invoke();
+                ChangeScene();
+            }
+        }
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("ARScene");
+    }
+}
---
+++ Assets/Scenes/PlayerChecker.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerChecker : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
